2859b9da116e84678dd020b785ca8f95e7880294
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-07-01 20:54:25 -0300

    scafolding + useeffect beta

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,37 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './bitStyle.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [valor, setValor] = useState('');
+  const [arrayPais, setArrayPais] = useState([]);
+
+  const handleInput = (e) => {
+    const superValue = e.target.value;
+    setValor(superValue);
+  };
+
+  const url = 'https://restcountries.com/v3.1/all';
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+      setArrayPais([response.data]);
+    });
+  }, []);
+
+  //tenes que hacer el map para que c/objeto sea un pais
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="body-font">
+      <p>mI aPIMUNDI</p>
+      <form>
+        <label className="label-size">find countries: </label>
+        <input type="text" onChange={handleInput} />
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
